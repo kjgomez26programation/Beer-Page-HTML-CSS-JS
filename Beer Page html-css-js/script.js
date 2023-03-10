a4fedb1fee14dc760023f2d2255f58448dda1578
kjgomez26programation <118693498+kjgomez26programation@users.noreply.github.com>
2023-03-10 16:11:25 -0500

    Add files via upload

---
+++ Beer Page html-css-js/script.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".mySwiper-1",{
+    slidesPerView:1,
+    spaceBetween:30,
+    loop:true,
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+
+    },
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    }
+});
+
+
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView:4,
+    spaceBetween:30,
+    loop:true,
+    loopFillGrounpWithBlank:true,
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    breakpoints: {
+        0:{
+            slidesPerView:1
+        },
+        520:{
+            slidesPerView:2
+        },
+        950:{
+            slidesPerView:3
+        }
+    }
+});
